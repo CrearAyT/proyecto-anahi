6dcb47762e2bf61d05732cacd8cfc1f6c60677c5
Adrian Pardini <github@tangopardo.com.ar>
2011-11-03 17:06:49 -0300

    plugin ṕara testear PlotWindow

--- plugins/__init__.py
+++ plugins/__init__.py
@@ -15,3 +15,28 @@ class SinkTest(object):
 
     def destroy(self):
         print 'sink destroy'
+
+class PlotTest(object):
+    name = 'Test de PlotWindow solo'
+
+    def __init__(self):
+        from plotwindow import PlotWindow
+        self.plotw = PlotWindow()
+        self.plotw.add_curve('Chan 1')
+        self.plotw.add_curve('Chan 2')
+
+
+    def new_data(self, data, *args, **kwargs):
+        for item in data:
+            self.plotw.add_datapoint(item[0], *item[1])
+    
+    def start(self):
+        self.plotw.show()
+
+    def stop(self):
+        self.plotw.hide()
+
+    def destroy(self):
+        self.plotw.close()
+
+all = [SinkTest, PlotTest]
