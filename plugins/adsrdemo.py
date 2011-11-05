c45edc2f9a53760e0ddcd6616c129be833ff220c
Adrian Pardini <github@tangopardo.com.ar>
2011-11-05 12:51:14 -0300

    nuevo pugin: demo completo adsr (falta conexion a vlc)

---
+++ plugins/adsrdemo.py
@@ -0,0 +1,85 @@
+# -*- coding: utf-8 -*-
+from PyQt4.QtCore import *
+from PyQt4.QtGui import *
+import PyQt4.Qwt5 as Qwt
+
+from plotwindow import PlotWindow
+from utils import lpfilt, diff, adsr
+
+class ADSRDemo(QMainWindow):
+    name = 'Prueba ADSR'
+
+    def __init__(self, parent=None):
+        super(ADSRDemo, self).__init__(parent)
+
+        self.plotw = PlotWindow(self)
+        self.plotw.add_curve('Chan 1')
+        self.plotw.add_curve('dChan1 / dT')
+        self.plotw.add_curve('Umbral dC1/dT')
+        self.plotw.add_curve('Detectado')
+        self.plotw.add_curve('ADSR')
+
+        self.dx = diff()
+
+        self.adsr = adsr()
+
+        # atributo adsr : [nombre ,                    min, max,  k,       kdisp,   default]
+        self.params = {  'attackl':['Duracion Ataque', 0,   200,  1./10,   1./100,  150 ],
+                         'sustl':['Duracion Sustain',  0,   900,  1,       1./10,   100 ],
+                         'rell':['Duracion Release',   0,   300,  1,       1./10,   15  ],
+                         'alfa_att':['alfa Ataque',    0,   1000, 1./1000, 1./1000, 300 ],
+                         'alfa_sus':['alfa Sustain',   0,   1000, 1./1000, 1./1000, 850 ],
+                         'alfa_rel':['alfa Release',   0,   1000, 1./1000, 1./1000, 850 ],
+                         'umbral':['umbral deteccion', 0,   400,  1,       1,       100 ]
+                       }
+
+
+        self.main_frame = QWidget()
+        lay = QGridLayout()
+        vbox = QVBoxLayout()
+        lay.addLayout(vbox, 0,0)
+        lay.setColumnStretch(1,1)
+        lay.addWidget(self.plotw,0,1)
+        self.main_frame.setLayout(lay)
+        self.setCentralWidget(self.main_frame)
+
+        for attr,params  in self.params.iteritems():
+            (nom, xmin, xmax, k, kdisp, default) = params
+            lbl = QLabel(nom)
+            sld = QSlider(Qt.Horizontal)
+            sld.setRange(xmin, xmax)
+            sld.valueChanged[int].connect(self.set_param)
+            sld.params = (nom, lbl, attr, k, kdisp)
+            sld.setValue(default) 
+            vbox.addWidget(lbl)
+            vbox.addWidget(sld)
+
+        vbox.addStretch(1)
+
+        self.setWindowTitle('Prueba ADSR')
+
+
+    def set_param(self, value):
+        (nom, lbl, attr, k, kdisp) = self.sender().params
+        val = k*value
+        lbl.setText(nom + ': %.3f'%(value*kdisp) )
+        setattr(self.adsr, attr, val) 
+
+    def new_data(self, data, *args, **kwargs):
+        umb = self.adsr.umbral
+        for item in data:
+            x = item[1][0]
+            d = self.dx(x)
+            ok = 150*(abs(d)>=umb)
+            ad = self.adsr(x)
+            self.plotw.add_datapoint(item[0], x, d, umb, ok, ad)
+ 
+    def start(self):
+        self.show()
+
+    def stop(self):
+        self.hide()
+
+    def destroy(self):
+        self.close()
+
