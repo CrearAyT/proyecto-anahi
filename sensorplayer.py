7239cd515c5f3bae5df934cff0476ed3dd194add
Adrian Pardini <github@tangopardo.com.ar>
2011-11-30 19:37:49 -0300

    sensorplayer: primer bosquejo de clase principal

--- sensorplayer.py
+++ sensorplayer.py
@@ -24,3 +24,70 @@ class Playlist(list):
                 return inner
             setattr(self, n, wrapper(n))
 
+
+class SensorPlayer(object):
+    def __init__(self):
+
+        self._playlist = Playlist()
+        self._playlist.changed.connect(self.__update_playlist)
+
+        self._vlc = None
+        self._repeat = False
+        self._loop = False
+
+        self.adsr = adsr()
+
+
+
+    @property
+    def playlist(self):
+        return self._playlist
+
+    def __update_playlist(self):
+        if self._vlc is None:
+            return
+
+        #FIXME: borrar lista del _vlc y reemplazar por la nueva
+        for f in self._playlist:
+            self._vlc.addfile(f)
+
+    def start(self):
+        if self._vlc is None:
+            self._vlc = VLCProcess(gui=True)
+            self.__update_playlist()
+        self._vlc.play()
+
+    def stop(self):
+        if self._vlc is not None:
+            self._vlc.stop()
+
+    @property
+    def repeat(self):
+        """Si True, reproducir indefinidamente el archivo actual"""
+        return self._repeat
+
+    @repeat.setter
+    def repeat(self, what):
+        if what:
+            self._repeat = True
+        else:
+            self._repeat = False
+
+        if self._vlc:
+            self._vlc.repeat(what)
+
+    @property
+    def loop(self):
+        """Si True, reproducir indefinidamente la lista de archivos"""
+        return self._loop
+
+    @loop.setter
+    def loop(self, what):
+        if what:
+            self._loop = True
+        else:
+            self._loop = False
+
+        if self._vlc:
+            self._vlc.loop(what)
+
